9521b706bf9d75b0aadf9b9f9d3efae37278ad26
Dinakrushna7077 <dinakrushnamohanta7077@gmail.com>
2025-05-11 21:42:00 +0530

    Add project files.

---
+++ SB INFOTECH/Scripts/Script.js
@@ -0,0 +1,12 @@
+﻿const menubtn = document.getElementById("btn");
+menubtn.addEventListener("click", () => {
+    const dropdown = document.getElementById("drdn");
+    if (dropdown.classList.contains("d-flex")) {
+        dropdown.classList.add("d-none");
+        dropdown.classList.remove("d-flex");
+    }
+    else {
+        dropdown.classList.remove("d-none");
+        dropdown.classList.add("d-flex", "dropdown");
+    }
+})
